a8774568edff5baff69f65483efb7e119b444ce4
aungaungNMK <arkarwunna890.zz@gmail.com>
2018-03-15 09:59:12 +0630

    day4

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_page_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails"
   end
 
   test "should get help" do
