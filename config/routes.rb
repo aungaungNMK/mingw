c626c2937ec1c837f55b63cd851dd427514e7cb7
aungaungNMK <arkarwunna890.zz@gmail.com>
2018-03-15 08:48:50 +0630

    static page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_page/help'
 
+  get 'static_page/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_page#home'
 end
